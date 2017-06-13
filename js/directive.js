acd9fcfd2cacb8a1170330c4c3c86c4e8481ea47
Brobbiethao <Brobbiethao@gmail.com>
2017-06-13 14:13:06 -0500

    directives-added

--- js/directive.js
+++ js/directive.js
@@ -1,1 +1,9 @@
-
+angular.module('app')
+.directive("directive", function() {
+  return {
+    templateUrl: '../views/homepage.html',
+    templateUrl: '../views/pilots.html',
+    templateUrl: '../views/character.html',
+    restrict: 'A'
+  }
+});
